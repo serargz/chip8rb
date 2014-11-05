ac5b9248bb44b9ef9bc652d3bcca53b82fab1e17
Sergio Aristizábal Gómez <serargz@gmail.com>
2014-11-05 09:34:54 -0500

    Emulator class added.
    This class will handle all the emulation process.

---
+++ lib/chip8rb/emulator.rb
@@ -0,0 +1,59 @@
+module Chip8rb
+
+  # This CHIP-8 emulator is based on the instructions given in:
+  # http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
+  class Emaulator
+
+    attr_accessor :opcode
+
+    def initialize
+      # Chip-8 has 4k memory (0x100)
+      @memory = Array.new(4096)
+
+      # CPU Registers:
+      # . 0-E general purpose
+      # . F carry flag
+      @v = Array.new(16)
+
+      # Index register
+      @i = 0
+
+      # Program counter (instruction pointer)
+      @pc = 0
+
+      # The graphics are black and white and the screen has
+      # a total of 2048 (64x32)
+      @gfx = Array.new(64*32)
+
+      # Timer registers, count at 60Hz.
+      # When set above zero, they will count down to zero.
+      @delay_timer = 0
+
+      # The system's buzzer sounds whenever the sound
+      # timer reaches zer.
+      @sound_timer = 0
+
+      # Stack. The system has 16 levels of stack.
+      @stack = Array.new(16)
+
+      # Stack pointer
+      @sp = 0
+
+      # HEX based keypad.
+      @key = Array.new(16)
+    end
+
+    def emulate
+      # 1. Setup graphics
+      # 2. Setup input
+      # 3. Initialize / Load program into memory
+
+      # 4. Emulation loop
+      loop do
+        # 5. Emulate one cycle
+        # 6. If the draw flag is set, update screen
+        # 7. Update key press state (Press and Release)
+      end
+    end
+  end
+end
