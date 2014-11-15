bb60d46fab85b785ae817073e16bc82b0ca1542c
Sergio Aristizábal Gómez <serargz@gmail.com>
2014-11-15 17:07:10 -0500

    Fixed gtk screen initial window.
    The "drawing area" was not being added to the window, and other fixes.

--- lib/chip8rb/gtk_screen.rb
+++ lib/chip8rb/gtk_screen.rb
@@ -11,20 +11,24 @@ class GTKScreen < Screen
     @tile_size = 10
   end
 
-  def run(title)
+  def run
     # TODO:
     # Draw the initial screen and capture keyboard events
-    window = Gtk::Window.new
+    @window = Gtk::Window.new
 
     area = Gtk::DrawingArea.new
-    area.set_size_request(100,100)
+    area.set_size_request(@width*@tile_size, @height*@tile_size)
     area.signal_connect("expose_event") do
       alloc = area.allocation
       area.window.draw_arc(area.style.fg_gc(area.state), true, 0, 0, alloc.width, alloc.height, 0, 64 * 360)
     end
 
-    window.show
-    window.set_default_size(@width*@tile_size, @height*@tile_size).show_all
+    @window.add(area)
+    @window.show
+    @window.set_default_size(@width*@tile_size, @height*@tile_size).show_all
+    @window.signal_connect "destroy" do
+      Gtk.main_quit
+    end
 
     Gtk.main
   end
